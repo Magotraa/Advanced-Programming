fe8108449e01034dd30d7766778d62dc80367c48
Aryan <Arjun.magotra.india@gmail.com>
2016-10-06 12:42:29 +0900

    changes..additiona classes.

--- Source2_week5.cpp
+++ Source2_week5.cpp
@@ -36,95 +36,87 @@ void draw()
 			pixels[(i + width *j) * 3 + 2] = 1.0f;
 
 		}
-
-
-	
-
 }
 
 
 
+GeometricObjects  **my_objects = new GeometricObjects*[6];
+
+
+
+	int main(void) {
+
+
+		my_objects[0] = new Line();
+		my_objects[1] = new Circle();
+		my_objects[2] = new Box();
+		my_objects[3] = new Triangle();
+		my_objects[4] = new Polygon();
+
+
+
+
+		/*std::vector<GeometricObjects*> go_vector;
+
+		go_vector.push_back(new Circle);
+		go_vector.push_back(new Box);*/
+
+		// triangles, polygons, diamond, rohmbus.......
+
+		/*for (auto itr : go_vector)
+			(itr)->draw();*/
+
+
+
+		GLFWwindow* window;
+
+		/* Initialize the library */
+		if (!glfwInit())
+			return -1;
+
+		/* Create a windowed mode window and its OpenGL context */
+		window = glfwCreateWindow(width, height, "Week 5: polymorphism", NULL, NULL);
+		if (!window)
+		{
+			glfwTerminate();
+			return -1;
+		}
 
 
 
 
 
+		/* Make the window's context current */
+		glfwMakeContextCurrent(window);
+
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			glfwGetCursorPos(window, &win_x, &win_y);
+
+			draw();
 
 
 
+			glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels);
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
 
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
 
-
-
-
-
-
-
-
-
-int main(void) {
-
-	/*std::vector<GeometricObjects*> go_vector;
-
-	go_vector.push_back(new Circle);
-	go_vector.push_back(new Box);*/
-
-	// triangles, polygons, diamond, rohmbus.......
-
-	/*for (auto itr : go_vector)
-		(itr)->draw();*/
-
-
-
-	GLFWwindow* window;
-
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(width, height, "Week 5: polymorphism", NULL, NULL);
-	if (!window)
-	{
 		glfwTerminate();
-		return -1;
+
+		delete[] pixels;
+
+		return 0;
+
+
 	}
-
-
-
-
-
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwGetCursorPos(window, &win_x, &win_y);
-
-		draw();
-
-
-
-		glDrawPixels(width, height, GL_RGB, GL_FLOAT, pixels);
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
-
-	delete[] pixels;
-
-	return 0;
-
-
-}
 
 
 
